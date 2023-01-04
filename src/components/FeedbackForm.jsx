81eda6cb3bed8b0537d2bdae47e29bfa8e3ed4c1
Samuel <mucasantos@gmail.com>
2023-01-04 20:20:17 -0300

    First

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,21 +1,34 @@
+
 import { useState } from 'react'
+import Button from './shared/Button'
 import Card from './shared/Card'
 
 function FeedbackForm() {
 
-    const [text, setText] = useState('')
+  const [text, setText] = useState('')
+
+  const handleTextChange = (e) =>{
+
+    setText(e.target.value)
+
+  }
   return (
     <Card>
 
-<form>
-    <h2>O que você achou do aplicativo?</h2>
-    {/* @todo - rating select component*/}
+      <form>
+        <h2>O que você achou do aplicativo?</h2>
+        {/* @todo - rating select component*/}
 
-    <div className="input-group">
-        <input type="text" placeholder='Escreva a sua opinião'/>
-        <button type="submit">Enviar</button>
-    </div>
-</form>
+        <div className="input-group">
+          <input 
+            onChange={handleTextChange} 
+            type="text" 
+            placeholder='Escreva a sua opinião'
+            value={text}
+          />
+          <Button type="submit" >Enviar </Button>
+        </div>
+      </form>
 
     </Card>
   )

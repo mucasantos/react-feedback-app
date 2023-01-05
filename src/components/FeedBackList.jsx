99a7f861dc42daf9ee338061dc05bbf4b053939d
Samuel <mucasantos@gmail.com>
2023-01-05 14:41:18 -0300

    Refactor code --> using Array.from or for, instead of repeat code

--- src/components/FeedBackList.jsx
+++ src/components/FeedBackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import React from 'react'
 import FeedBackItem from './FeedBackItem'
 import PropTypes from "prop-types"
@@ -9,17 +10,39 @@ function FeedBackList({ feedback, handleDelete }) {
   }
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => {
-        return <FeedBackItem key={item.id} item={item} handleClick = {handleDelete} > </FeedBackItem>
-      })}
 
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}    
+            >
+            <FeedBackItem key={item.id} item={item} handleClick={handleDelete} /> 
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  /*
+    return (
+      <div className='feedback-list'>
+        {feedback.map((item) => {
+          return <FeedBackItem key={item.id} item={item} handleClick = {handleDelete} > </FeedBackItem>
+        })}
+  
+      </div>
+    )
+  
+  */
+
 }
 FeedBackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -9,121 +9,57 @@ function RatingSelect({ select }) {
         setSelected(+e.currentTarget.value)
         select(+e.currentTarget.value)
     }
-    return (
-        <ul className='rating'>
-            <li>
+
+    // Creating component with list
+
+    let list = []
+
+    for (let index = 0; index < 11; index++) {
+
+        list.push(
+            <li key={index}>
                 <input
                     type='radio'
-                    id='num1'
+                    id={`num${index}`}
                     name='rating'
-                    value='1'
+                    value={index}
                     onChange={handleChange}
-                    checked={selected === 1}
+                    checked={selected === index}
                 />
-                <label htmlFor='num1'>1</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num2'
-                    name='rating'
-                    value='2'
-                    onChange={handleChange}
-                    checked={selected === 2}
-                />
-                <label htmlFor='num2'>2</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num3'
-                    name='rating'
-                    value='3'
-                    onChange={handleChange}
-                    checked={selected === 3}
-                />
-                <label htmlFor='num3'>3</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num4'
-                    name='rating'
-                    value='4'
-                    onChange={handleChange}
-                    checked={selected === 4}
-                />
-                <label htmlFor='num4'>4</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num5'
-                    name='rating'
-                    value='5'
-                    onChange={handleChange}
-                    checked={selected === 5}
-                />
-                <label htmlFor='num5'>5</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num6'
-                    name='rating'
-                    value='6'
-                    onChange={handleChange}
-                    checked={selected === 6}
-                />
-                <label htmlFor='num6'>6</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num7'
-                    name='rating'
-                    value='7'
-                    onChange={handleChange}
-                    checked={selected === 7}
-                />
-                <label htmlFor='num7'>7</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num8'
-                    name='rating'
-                    value='8'
-                    onChange={handleChange}
-                    checked={selected === 8}
-                />
-                <label htmlFor='num8'>8</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num9'
-                    name='rating'
-                    value='9'
-                    onChange={handleChange}
-                    checked={selected === 9}
-                />
-                <label htmlFor='num9'>9</label>
-            </li>
-            <li>
-                <input
-                    type='radio'
-                    id='num10'
-                    name='rating'
-                    value='10'
-                    onChange={handleChange}
-                    checked={selected === 10}
-                />
-                <label htmlFor='num10'>10</label>
+                <label htmlFor={`num${index}`}>{index}</label>
             </li>
 
+        )
+    }
+
+
+    return (
+        <ul className='rating'>
+            {list}
         </ul>
     )
 }
 
 export default RatingSelect
+
+/*
+
+//O trecho de código abaixo funciona també, no lugar do FOR
+// sendo colocado no lugar de {list}
+{Array.from({ length: 10 }, (_, index) =>
+
+                <li key={index}>
+                    <input
+                        type='radio'
+                        id={`num${index}`}
+                        name='rating'
+                        value={index}
+                        onChange={handleChange}
+                        checked={selected === index}
+                    />
+                    <label htmlFor={`num${index}`}>{index}</label>
+                </li>
+
+            )}
+
+*/
